5669d5eea554c144bc0cf7639985047aca25ccac
brycebien <brycebien@gmail.com>
2023-11-22 19:13:46 -0500

    record command implemented
    TODO: store into DB

--- src/utility.py
+++ src/utility.py
@@ -1,24 +1,71 @@
-from abc import abstractmethod
-from datetime import datetime
+from shlex import split as shlex_split
+from abc import ABC, abstractmethod
+import re
+from dateutil import parser
 
 
-class CommandHandler(object):
-    def __init__(self):
+class RecordValidator(object):
+    @staticmethod
+    def validate(record_in):
+        pattern = re.compile(r'^\S+\s+\S+\s+\S+\s+\'[^\']+\'\s+:\S+$')
+        return bool(pattern.match(record_in))
+    
+class DateParser(object):
+    @staticmethod
+    def parse(date_in):
+        return parser.parse(date_in).strftime("%Y/%m/%d")
+
+
+class TimeTracker(object):
+    def record(self, inputs):
+        print(inputs)
+        date, start_time, end_time, task, tag = inputs
+        #TODO: implement entry to SQLite
+        print(f"Recorded: {date} {start_time} - {end_time} - {task} - {tag}")
+
+    def query(self, parameter):
+        #TODO: implementation to query from SQLite
+        print(f"Query: {parameter}")
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self):
         pass
 
-class Record(CommandHandler):
-    def record(self, inputs):
-        self.checkCommand(inputs)
+class RecordCommand(Command):
+    def __init__(self, inputs):
+        self.time_tracker = TimeTracker()
+        self.record_validator = RecordValidator()
+        self.date_parser = DateParser()
+        self.input = inputs
 
-    def checkCommand(self, inputs):
-        checked_inputs = []
-        format = '%Y/%m/%d'
-        try:
-            datetime.strptime(inputs[0],format)
-            checked_inputs.append(datetime.strptime(inputs[0],format).date())
-        except ValueError:
-            print('Invalid Date Time Format please enter YYYY/MM/DD')
-            return None
+    def execute(self):
+        if self.record_validator.validate(self.input):
+            inputs = shlex_split(self.input)
+            date = self.date_parser.parse(inputs[0])
+            inputs[0] = date
+            self.time_tracker.record(inputs)
+        else:
+            print("Invalid record input format. Please use DATE FROM TO TASK TAG format")
 
+class Console(object):
+    def __init__(self):
+        self.time_tracker = TimeTracker()
+        self.record_validator = RecordValidator()
+        self.date_parser = DateParser()
+        self.commands = []
 
+    def addCommand(self, command):
+        self.commands.append(command)
 
+    def runCommands(self):
+        for command in self.commands:
+            command.execute()
+    
+    def processCommand(self, user_in):
+        if user_in.startswith("query "):
+            #TODO: implement query
+            pass
+        elif user_in.startswith("record "):
+            record_in = user_in[len("record "):]
+            self.addCommand(RecordCommand(record_in))
